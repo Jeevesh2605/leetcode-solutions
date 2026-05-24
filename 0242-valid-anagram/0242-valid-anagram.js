eabf51b250ff7bdcec3375bb94b6c45c01c273d6
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-05-24 19:35:49 +0530

    Time: 11 ms (76.66%), Space: 55.1 MB (85.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,22 +3,47 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
+// var isAnagram = function(s, t) {
 
-    if(s.length !== t.length) {
+//     if(s.length !== t.length) {
+//         return false;
+//     }
+
+//     let count = {};
+//     for(let char of s) {
+//         count[char] = (count[char] || 0) + 1;
+//     }
+//     for(let char of t) {
+
+//         if(!count[char]) {
+//             return false;
+//         }
+//         count[char]--;
+//     }
+//     return true;
+// };
+
+
+var isAnagram = function(s,t){
+    s = s.toLowerCase();
+    t = t.toLowerCase();
+    s = s.replaceAll(" ","");
+    t = t.replaceAll(" ","");
+    if(s.length!==t.length){
         return false;
     }
-
-    let count = {};
-    for(let char of s) {
-        count[char] = (count[char] || 0) + 1;
+    let counts = new Array(26).fill(0);
+    for(let i=0;i<s.length;i++){
+        counts[s.charCodeAt(i)-'a'.charCodeAt(0)]++;
     }
-    for(let char of t) {
-
-        if(!count[char]) {
+    for(let i=0;i<t.length;i++){
+        counts[t.charCodeAt(i)-'a'.charCodeAt(0)]--;
+    }
+    for(let count of counts){
+        if(count!==0){
             return false;
         }
-        count[char]--;
     }
     return true;
-};
+
+}
