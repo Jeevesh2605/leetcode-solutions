64b7f7786d73bdf1d204db5e71516f25c016bb7b
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-05-28 21:52:04 +0530

    Time: 159 ms (5.76%), Space: 46.8 MB (5.68%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,41 +1,13 @@
-class Solution1 {
-    public boolean isValid(String s) {
-        Map<Character, Character> map  = new HashMap<>();
-        map.put(')','(');
-        map.put('}','{');
-        map.put(']','[');
-        Stack<Character> stack  = new Stack<>();
-        for(char c: s.toCharArray()){
-            if(map.containsValue(c)){
-                stack.push(c);
-            } else if(map.containsKey(c)){
-                if(stack.isEmpty()|| map.get(c) != stack.pop()) {
-                    return false;
-                }
-            }
-        }
-        return stack.isEmpty();
-    }
-}
-
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
+        while(s.contains("()") ||
+        s.contains("{}")||
+        s.contains("[]")){
+            s = s.replace("()", "");
+            s = s.replace("{}", "");
+            s = s.replace("[]", "");
 
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) return false;
-
-                char top = stack.pop();
-                if ((c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '[')) {
-                    return false;
-                }
-            }
         }
-        return stack.isEmpty();
+        return s.length()==0;
     }
 }
