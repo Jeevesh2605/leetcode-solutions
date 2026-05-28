95a62b93a0fbe91fa218b9d4c4c1d60f2706705c
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-05-28 22:04:09 +0530

    Time: 4 ms (39.69%), Space: 43.4 MB (25.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,13 +1,32 @@
-class Solution {
-    public boolean isValid(String s) {
-        while(s.contains("()") ||
-        s.contains("{}")||
-        s.contains("[]")){
-            s = s.replace("()", "");
-            s = s.replace("{}", "");
-            s = s.replace("[]", "");
+// class Solution {
+//     public boolean isValid(String s) {
+//         while(s.contains("()") ||
+//         s.contains("{}")||
+//         s.contains("[]")){
+//             s = s.replace("()", "");
+//             s = s.replace("{}", "");
+//             s = s.replace("[]", "");
 
+//         }
+//         return s.length()==0;
+//     }
+// }
+
+
+class Solution{
+    public boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c == '('){
+                stack.push(')');
+            }else if(c == '{'){
+                stack.push('}');
+            }else if(c=='['){
+                stack.push(']');
+            }else if(stack.isEmpty() || stack.pop() !=c){
+                return false;
+            }
         }
-        return s.length()==0;
+        return stack.isEmpty();
     }
 }
