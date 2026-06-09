637e8929eee8cc312059b7895c16ba00b3148aaf
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-06-09 23:50:10 +0530

    Time: 0 ms (100%), Space: 54.8 MB (10.23%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let n = nums.length;
+    let l = 0;
+    let r = n-1;
+    while(l<r){
+        let mid = Math.floor(l+(r-l)/2);
+        if(nums[mid]>nums[r]){
+            l=mid+1;
+        }else{
+            r=mid;
+        }
+    }
+    return nums[r];
+};
