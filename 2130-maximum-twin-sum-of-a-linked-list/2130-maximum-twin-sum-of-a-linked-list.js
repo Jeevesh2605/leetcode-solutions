75b066bfa03d01084f0c5b11852ffbd66d5e0b46
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-06-12 19:23:07 +0530

    Time: 3 ms (98.93%), Space: 82.1 MB (48.75%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.js
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.js
@@ -9,20 +9,50 @@
  * @param {ListNode} head
  * @return {number}
  */
-var pairSum = function(head) {
-    let arr = [];
-    let curr = head;
-    while(curr!==null){
-        arr.push(curr.val);
-        curr = curr.next;
+// var pairSum = function(head) {
+//     let arr = [];
+//     let curr = head;
+//     while(curr!==null){
+//         arr.push(curr.val);
+//         curr = curr.next;
+//     }
+//     let res = 0;
+//     let i=0;
+//     let j=arr.length-1;
+//     while(i<j){
+//         res = Math.max(res,arr[i]+arr[j]);
+//         i++;
+//         j--;
+//     }
+//     return res;
+// };
+
+
+var pairSum = function(head){
+    // find mid
+    let slow = head;
+    let fast = head;
+    while(fast!==null && fast.next!==null){
+        slow = slow.next;
+        fast = fast.next.next;
     }
+    let mid = slow;
+    // reverse 2nd half
+    let prev = null;
+    let nextNode = null;
+    while(mid!==null){
+        nextNode = mid.next;
+        mid.next = prev;
+        prev = mid;
+        mid = nextNode;
+    }
+    // find max result
     let res = 0;
-    let i=0;
-    let j=arr.length-1;
-    while(i<j){
-        res = Math.max(res,arr[i]+arr[j]);
-        i++;
-        j--;
+    let curr = head;
+    while(prev!==null){
+        res = Math.max(res, curr.val + prev.val);
+        curr = curr.next;
+        prev = prev.next;
     }
     return res;
-};
+}
