b6c960d5b33a78cd4abf911865965eb17badddce
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-06-12 10:47:30 +0530

    Time: 2 ms (31.41%), Space: 55.8 MB (60.74%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
@@ -3,14 +3,31 @@
  * @param {number} target
  * @return {number}
  */
-var countPairs = function(nums, target) {
+// var countPairs = function(nums, target) {
+//     let res = 0;
+//     for(let i=0;i<nums.length;i++){
+//         for(let j=i+1;j<nums.length;j++){
+//             if(nums[i]+nums[j]<target){
+//                 res++;
+//             }
+//         }
+//     }
+//     return res;
+// };
+
+
+var countPairs = function(nums,target){
     let res = 0;
-    for(let i=0;i<nums.length;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]<target){
-                res++;
-            }
+    nums.sort((a,b)=> a-b);
+    let l=0;
+    let r = nums.length-1;
+    while(l!==r){
+        if(nums[l]+nums[r]<target){
+            res+=r-l;
+            l++;
+        }else{
+            r--;
         }
     }
     return res;
-};
+}
