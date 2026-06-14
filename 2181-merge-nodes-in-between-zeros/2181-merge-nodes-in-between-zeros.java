cdca737f67857d9e993b9cf4813e6d13efda1484
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-06-14 23:52:48 +0530

    Time: 9 ms (9.23%), Space: 230.5 MB (69.94%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -8,21 +8,42 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode mergeNodes(ListNode head) {
+//         ListNode p1 = head.next;
+//         ListNode p2 = p1;
+//         while(p2!=null){
+//             int sum=0;
+//             while(p2!=null && p2.val!=0){
+//                 sum+=p2.val;
+//                 p2 = p2.next;
+//             }
+//             p1.val = sum;
+//             p2 = p2.next;
+//             p1.next = p2;
+//             p1 = p1.next;
+//         }
+//         return head.next;
+//     }
+// }
+
+
+
+
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        ListNode p1 = head.next;
-        ListNode p2 = p1;
-        while(p2!=null){
-            int sum=0;
-            while(p2!=null && p2.val!=0){
-                sum+=p2.val;
-                p2 = p2.next;
-            }
-            p1.val = sum;
-            p2 = p2.next;
-            p1.next = p2;
-            p1 = p1.next;
+        head = head.next;
+        if(head==null){
+            return head;
         }
-        return head.next;
+        ListNode temp = head;
+        int sum=0;
+        while(temp!=null && temp.val!=0){
+            sum+=temp.val;
+            temp=temp.next;
+        }
+        head.val = sum;
+        head.next = mergeNodes(temp);
+        return head;
     }
-}
+}    
