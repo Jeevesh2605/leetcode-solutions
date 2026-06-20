bce8df55cfde4795badb650889a13a3bc288dfb1
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-06-20 10:40:19 +0530

    Time: 0 ms (100%), Space: 55.6 MB (21.75%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let n = nums.length;
+    for(let i=1;i<n;i++){
+        nums[i]+=nums[i-1];
+    }
+    return nums;
+};
