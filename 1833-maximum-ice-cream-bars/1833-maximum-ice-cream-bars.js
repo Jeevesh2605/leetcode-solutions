143a3a9acdecb433d639385c6f9eb0318dd4f6d0
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-06-21 18:21:12 +0530

    Time: 189 ms (9.09%), Space: 69.6 MB (25%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} costs
+ * @param {number} coins
+ * @return {number}
+ */
+var maxIceCream = function(costs, coins) {
+    costs.sort((a,b)=>a-b);
+    let count = 0;
+    for(let cost of costs){
+        if(cost>coins){
+            return count;
+        }else{
+            count++;
+            coins-=cost;
+        }
+    }
+    return count;
+};
