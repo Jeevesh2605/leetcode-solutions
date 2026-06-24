0f27fd3c71a79c0f3f2a71d0c09771dd75c3410b
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-06-24 22:31:25 +0530

    Time: 137 ms (67.59%), Space: 111.4 MB (70.3%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,31 +1,25 @@
 class MedianFinder {
-    PriorityQueue<Integer> leftMaxHeap = new PriorityQueue<>(Collections.reverseOrder());
-    PriorityQueue<Integer> rightMinHeap = new PriorityQueue<>();
-
+    private PriorityQueue<Integer> leftMaxHeap; // max heap
+    private PriorityQueue<Integer> rightMinHeap; // min heap
     public MedianFinder() {
-        
-    } 
+        leftMaxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        rightMinHeap = new PriorityQueue<>();
+    }
+    
     public void addNum(int num) {
-        if(leftMaxHeap.isEmpty() || num < leftMaxHeap.peek()){
+        if(leftMaxHeap.isEmpty() || num<leftMaxHeap.peek()){
             leftMaxHeap.add(num);
-        } else{
-            rightMinHeap.add(num);
-        }
-        // always maintain leftMaxHeap size 1 gretaer than rightMinHeap size
-        // ya fir dono ka size equal ho
-        if(Math.abs(leftMaxHeap.size() - rightMinHeap.size())>1){
-            rightMinHeap.add(leftMaxHeap.peek());
-            leftMaxHeap.poll();
-        } else if(leftMaxHeap.size()<rightMinHeap.size()){
-            leftMaxHeap.add(rightMinHeap.peek());
-            rightMinHeap.poll();
+        }else rightMinHeap.add(num);
+        if(Math.abs(leftMaxHeap.size()-rightMinHeap.size())>1){
+            rightMinHeap.add(leftMaxHeap.poll());
+        }else if(leftMaxHeap.size()<rightMinHeap.size()){
+            leftMaxHeap.add(rightMinHeap.poll());
         }
     }
     
     public double findMedian() {
-        if(leftMaxHeap.size() == rightMinHeap.size()){
-            double mean = (leftMaxHeap.peek()+rightMinHeap.peek())/2.0;
-            return mean;
+        if(leftMaxHeap.size()==rightMinHeap.size()){
+            return (leftMaxHeap.peek()+rightMinHeap.peek())/2.0;
         }
         return leftMaxHeap.peek();
     }
