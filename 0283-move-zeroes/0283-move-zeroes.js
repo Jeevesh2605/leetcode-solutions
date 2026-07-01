391224dfe6447425b937e409f15bb50605b18202
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-07-02 00:44:56 +0530

    Time: 1 ms (76.02%), Space: 58.2 MB (97.52%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -6,7 +6,7 @@ var moveZeroes = function(nums) {
     let insertPosition = 0;
     for(let i=0;i<nums.length;i++){
         if(nums[i]!==0){
-            nums[insertPosition]=nums[i];
+            nums[insertPosition] = nums[i];
             insertPosition++;
         }
     }
