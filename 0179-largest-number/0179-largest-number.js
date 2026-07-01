f4f3e23072f09ce8754fd32b79f435fc9bfb1524
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-07-02 00:39:49 +0530

    Time: 0 ms (100%), Space: 55.4 MB (75.56%) - LeetHub

--- 0179-largest-number/0179-largest-number.js
+++ 0179-largest-number/0179-largest-number.js
@@ -3,14 +3,13 @@
  * @return {string}
  */
 var largestNumber = function(nums) {
-    nums.sort((a, b) => {
-        let ab = String(a) + String(b);
-        let ba = String(b) + String(a);
-
-        if (ab > ba) return -1;
-        if (ab < ba) return 1;
+    nums.sort((a,b)=>{
+        let ab = String(a)+String(b);
+        let ba = String(b)+String(a);
+        if(ab>ba) return -1;
+        if(ab<ba) return 1;
         return 0;
     });
     let result = nums.join('');
-    return result[0] ==='0'? '0' : result;
+    return result[0]==='0'?'0':result;
 };
