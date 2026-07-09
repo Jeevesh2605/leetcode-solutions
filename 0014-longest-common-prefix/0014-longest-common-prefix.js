7ad8fc9f5c1cfcea25e9302454c050e8c51077c0
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-07-09 10:12:13 +0530

    Time: 0 ms (100%), Space: 53.9 MB (75.48%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(!strs || strs.length===0) return "";
+    let prefix  = strs[0];
+    for(let i=0;i<strs.length;i++){
+        while(!strs[i].startsWith(prefix)){
+            prefix = prefix.substring(0, prefix.length-1);
+            if(prefix ==="") return "";
+        }
+    }
+    return prefix;
+};
