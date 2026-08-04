49d62115d24c54e40c152c4223dc77c0cce88b8e
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-08-04 21:39:28 +0530

    Time: 0 ms (100%), Space: 47.4 MB (49.29%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,10 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int allXOR = 0;
-        for(int i=0; i<=nums.length;i++){
+        for(int i=0;i<=nums.length;i++){
             allXOR = allXOR^i;
         }
-        for(int num: nums){
+        for(int num:nums){
             allXOR = allXOR^num;
         }
         return allXOR;
